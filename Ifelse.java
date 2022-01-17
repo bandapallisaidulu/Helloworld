6c7d46d22489d739438a9ce2653ecea26f0eb72b
bandapallisaidulu <97540359+bandapallisaidulu@users.noreply.github.com>
2022-01-17 15:38:58 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,27 @@
+package Firsthand_Java;
+
+public class Ifelse {
+	public static void main (String args[]) {
+		int num1 = 200;
+		int num2 = 1000;
+		int num3 = 2000;
+		
+		if(num1 > num2) {
+			System.out.print(num1);
+			
+		
+			
+		}
+		else if (num2 > num3){ 
+			System.out.print(num2);
+			
+			
+		}
+		else {
+			System.out.print(num3);
+		}
+
+		
+	}
+
+}
